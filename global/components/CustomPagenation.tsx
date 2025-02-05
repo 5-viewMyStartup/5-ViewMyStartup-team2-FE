6eb42110166d6c9fce18de169a0b5256d8e8aea0
josunkim <32131319+josunkim@users.noreply.github.com>
2025-02-05 17:42:00 +0900

    feat : 페이지 네이션 추가

---
+++ global/components/CustomPagenation.tsx
@@ -0,0 +1,40 @@
+import { Pagination } from "@mui/material";
+import { colorChips } from "../styles/colorChips";
+
+interface CustomPagenationProps {
+  page: number; // 현재 페이지
+  count: number; // 전체 페이지 수
+  handleChange: (event: React.ChangeEvent<unknown>, value: number) => void;
+}
+/*
+ex)
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+   setPage(value);
+  };
+ */
+export const CustomPagenation: React.FC<CustomPagenationProps> = ({
+  page,
+  count,
+  handleChange,
+}) => {
+  return (
+    <Pagination
+      defaultPage={1}
+      page={page}
+      count={count}
+      onChange={handleChange}
+      variant="outlined"
+      shape="rounded"
+      sx={{
+        "& .MuiPaginationItem-root": {
+          backgroundColor: colorChips.black_100, // 버튼 배경색
+          color: colorChips.gray_200, // 버튼의 텍스트 색상
+          "&.Mui-selected": {
+            backgroundColor: colorChips.brand_orange, // 선택된 버튼 배경색
+            color: colorChips.white, // 선택된 버튼의 텍스트 색상
+          },
+        },
+      }}
+    />
+  );
+};
