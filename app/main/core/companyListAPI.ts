ef9b22bbfc75268cda65dacff170c5232fa948c0
HoeunRyu <hoeun9565@gmail.com>
2025-02-11 15:37:17 +0900

    feat: main 검색, 필터 기능

--- app/main/core/companyListAPI.ts
+++ app/main/core/companyListAPI.ts
@@ -17,7 +17,6 @@ export const getCompanyListAPI = async (
   //쿼리 기본값
   const { page = 1, search = "", filter = "revenueDesc" } = params;
 
-  //FIXME: 지금 엔드포인트는 세정님이 만드신 api로 연결되는데, 재완님한테 검색키워드랑 필터, 페이지 쿼리 받아서 카테고리도 추가 + 지원자수도 계산하고  totalpages, page까지 같이 반환해주는걸로 api짜달라고 하기
   try {
     const response: AxiosResponse<CompanyListResponse> = await instance.get(
       "/api/main/companies",
