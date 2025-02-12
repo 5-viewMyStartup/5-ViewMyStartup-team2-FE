1cb8cfe808606dd38af6ce4f6d7ece40b0c6d101
LenaC-web <lena.programming@gmail.com>
2025-02-13 02:07:48 +0900

    feat/bookmark 페이지네이션 추가, 목데이터 추가하여 보이기

--- app/bookmark/features/listTitle/feature.tsx
+++ app/bookmark/features/listTitle/feature.tsx
@@ -7,6 +7,13 @@ export default function ListTitle(): React.ReactElement {
   return (
     <Stack sx={listHeaderContainerStyle}>
       <Typo className="text_B_20" content="즐겨찾기" color={colorChips.white} />
+      <Box sx={sortBoxStyle}>
+          <Typo
+            className="text_SB_20"
+            content="정렬"
+            color={colorChips.white}
+          />
+        </Box>
     </Stack>
   );
 }
@@ -21,13 +28,6 @@ const listHeaderContainerStyle = {
   pl: { xs: "16px", sm: "0" },
 };
 
-const searchBoxStyle = {
-  display: "flex",
-  alignItems: "center",
-  width: ["189px", "269px", "448px"],
-  height: ["40px", "48px"],
-  border: `1px solid ${colorChips.white}`, //border는 컴포넌트 넣고 삭제
-};
 
 const sortBoxStyle = {
   display: "flex",
