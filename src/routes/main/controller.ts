5f407416cb1d62a95efa72722d15ad6687df18c6
BBong99 <qhdwodhks11230@gmail.com>
2025-02-14 16:36:06 +0900

    fix: remove 백엔드src

--- src/routes/main/controller.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import getMainCompanyList from "./service";
-
-const router = express.Router();
-
-router.get("/companies", getMainCompanyList);
-
-export default router;
