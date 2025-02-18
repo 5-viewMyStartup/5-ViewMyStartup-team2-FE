9891671a235ea146fda0940a88ddd3dd8ecfed69
sejeong <ygn06024@naver.com>
2025-02-19 06:25:08 +0900

    Fix: 배포 오류 수정...2

--- app/company-comparison-page/features/listTitle/companyFeature.tsx
+++ app/company-comparison-page/features/listTitle/companyFeature.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Stack, Box } from "@mui/material";
+import { Stack, Box, CircularProgress } from "@mui/material";
 import { colorChips } from "@/global/styles/colorChips";
 import { Typo } from "@/global/styles/Typo";
 import { CompanySelectModal } from "./components/CompanySelectModal"; // 기업 선택 모달 컴포넌트
-import { ComparisonCompanyDTO } from "@/global/types/data-contracts"; // 기업 데이터 타입
 import { useCompanyFetch } from "../../core/useCompanyFetchHook"; // `useCompanyFetch` 훅 import
 import { CompanyListQuery } from "@/global/types/data-contracts"; // 쿼리 파라미터 타입
 import { useRouter } from "next/navigation";
@@ -13,19 +12,17 @@ import { CompanyCard } from "@/global/components/CompanyCard";
 const CompanyListTitle: React.FC = () => {
   const [modalOpen, setModalOpen] = useState(false); // 모달 상태
   const [loading, setLoading] = useState(true); // 로딩 상태 관리
-  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태
   const router = useRouter();
 
   // ✅ zustand에서 상태 및 액션 가져오기
   const {
-    // selectedAppliedCompanies, // 최근 지원한 기업 목록
     selectedSearchCompanies, // 검색 결과에서 선택한 기업 목록
-    // selectSearchCompany,
+
     deselectSearchCompany,
   } = useCompanyStore();
 
   const params: CompanyListQuery = {
-    page: currentPage, // 현재페이지를 기준으로 API 요청
+    page: 1, // 현재페이지를 기준으로 API 요청
   };
 
   // `useCompanyFetch` 훅을 사용하여 기업 목록 불러오기
@@ -37,13 +34,6 @@ const CompanyListTitle: React.FC = () => {
     }
   }, [isLoading, companies]); // isLoading과 companies 상태에 따라 effect 실행
 
-  // 페이지가 변경될 때마다 호출되는 useEffect
-  useEffect(() => {
-    if (currentPage > 0) {
-      setLoading(true); // 페이지 변경 시 로딩 상태 true로 설정
-    }
-  }, [currentPage]); // currentPage 값 변경될 때마다 호출
-
   // 모달 열기
   const handleOpenModal = () => setModalOpen(true);
 
@@ -52,13 +42,6 @@ const CompanyListTitle: React.FC = () => {
 
   // ✅ 기본 이미지를 적용하는 함수
   const defaultImage = "@/public/assets/default-company-img.svg";
-  // const formatSelectedCompanies = (companies: ComparisonCompanyDTO[]) => {
-  //   return companies.map((company) => ({
-  //     ...company,
-  //     image: company.image || defaultImage, // 기본 이미지 적용
-  //     category: company.category || [],
-  //   }));
-  // };
 
   return (
     <Stack sx={{ width: "100%" }}>
@@ -95,64 +78,77 @@ const CompanyListTitle: React.FC = () => {
       </Stack>
 
       {/* 선택된 기업을 보여주는 두 번째 박스 */}
-      <Box
-        sx={{
-          minHeight: "300px",
-          backgroundColor: colorChips.black_300, // 배경색 지정
-          padding: "8px", // 여백 추가
-          borderRadius: "8px", // 모서리 둥글게
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        {/* 선택된 기업이 없을 때의 메시지 */}
-        {selectedSearchCompanies.length === 0 ? (
-          <Typo
-            className="text_R_14"
-            content={
-              "아직 추가한 기업이 없어요.\n버튼을 눌러 기업을 추가해보세요!"
-            }
-            color={colorChips.gray_200}
-            customStyle={{
-              textAlign: "center",
-              whiteSpace: "pre-line", // ✅ 개행 문자(\n)를 줄바꿈으로 처리
-            }}
-          />
-        ) : (
-          // 선택된 기업들 CompanyCard로 표시
-          <Stack
-            direction="row"
-            spacing={2}
-            sx={{
-              flexWrap: "nowrap",
-              justifyContent: "center",
-              gap: "16px",
-              width: "100%",
-              "@media (max-width: 743px)": {
-                flexWrap: "wrap",
-              },
-            }}
-          >
-            {selectedSearchCompanies.map((company, index) => (
-              <CompanyCard
-                key={index}
-                company={{
-                  image:
-                    company.image || "@/public/assets/default-company-img.svg", // 기본 이미지 처리
-                  name: company.name,
-                  category:
-                    company.category && company.category.length > 0
-                      ? company.category.join(", ")
-                      : "기타",
-                }}
-                onRemove={() => deselectSearchCompany(company)} // ✅ 기업 삭제 기능 추가
-                sx={companyCardStyles}
-              />
-            ))}
-          </Stack>
-        )}
-      </Box>
+      {/* ✅ 로딩 상태 표시 */}
+      {loading ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "300px",
+          }}
+        >
+          <CircularProgress color="inherit" />
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            minHeight: "300px",
+            backgroundColor: colorChips.black_300, // 배경색 지정
+            padding: "8px", // 여백 추가
+            borderRadius: "8px", // 모서리 둥글게
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {/* 선택된 기업이 없을 때의 메시지 */}
+          {selectedSearchCompanies.length === 0 ? (
+            <Typo
+              className="text_R_14"
+              content={
+                "아직 추가한 기업이 없어요.\n버튼을 눌러 기업을 추가해보세요!"
+              }
+              color={colorChips.gray_200}
+              customStyle={{
+                textAlign: "center",
+                whiteSpace: "pre-line", // ✅ 개행 문자(\n)를 줄바꿈으로 처리
+              }}
+            />
+          ) : (
+            // 선택된 기업들 CompanyCard로 표시
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{
+                flexWrap: "nowrap",
+                justifyContent: "center",
+                gap: "16px",
+                width: "100%",
+                "@media (max-width: 743px)": {
+                  flexWrap: "wrap",
+                },
+              }}
+            >
+              {selectedSearchCompanies.map((company, index) => (
+                <CompanyCard
+                  key={index}
+                  company={{
+                    image: company.image || defaultImage, // 기본 이미지 처리
+                    name: company.name,
+                    category:
+                      company.category && company.category.length > 0
+                        ? company.category.join(", ")
+                        : "기타",
+                  }}
+                  onRemove={() => deselectSearchCompany(company)} // ✅ 기업 삭제 기능 추가
+                  sx={companyCardStyles}
+                />
+              ))}
+            </Stack>
+          )}
+        </Box>
+      )}
       <Box
         sx={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
       >
